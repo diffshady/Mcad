75bc8965c29d41eb66847cabf163e863ca120ecf
Christian Abao <christianabao52@gmail.com>
2026-05-26 17:44:52 +0800

    chore: remove temporary proxy logging

--- api/proxy.js
+++ api/proxy.js
@@ -20,7 +20,6 @@ export default async function handler(req, res) {
     .filter((part) => part && !part.startsWith('path='))
     .join('&');
   const targetUrl = `${backendRoot}/${pathPart}${cleanQuery ? `?${cleanQuery}` : ''}`;
-  console.log(`[proxy] ${req.method} ${req.url} -> ${targetUrl}`);
 
   if (req.method === 'OPTIONS') {
     return res.status(204).end();
@@ -50,7 +49,6 @@ export default async function handler(req, res) {
       headers,
       body,
     });
-    console.log(`[proxy] upstream status ${response.status} for ${targetUrl}`);
 
     const contentType = response.headers.get('content-type') || '';
     res.status(response.status);
